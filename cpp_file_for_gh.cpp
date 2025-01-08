29b5019977d34b301b21e8c66622bbd40e3db220
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2025-01-07 21:55:16 -0700

    Adding cpp file for GH Languages percentage

---
+++ cpp_file_for_gh.cpp
@@ -0,0 +1,8 @@
+// Cliff note - I've burned hours and hours trying to get the GitHub Languages percentage
+// histogram to be correct with respect to having C++ files in the Presentations repo.
+//
+// So this file is simply a C++ file that doesn't really do anything.
+
+int main() {
+  return 0;
+}
